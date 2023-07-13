2c73aaf570c70adc070b58d81f765b20dd2cbed7
zox1r <qarshiyevzoxir@gmail.com>
2023-07-13 09:32:02 +0200

    bin/if03.dart

--- bin/if03.dart
+++ bin/if03.dart
@@ -6,4 +6,17 @@
         a: integer
     Returns:
         a: integer*/
-void main() {}
+int func(int number){
+if(number > 0){
+    return number + 1;
+} if(number < 0){
+    return number - 2;
+} if(number == 0){
+    return number * 10;
+}
+}
+
+
+void main() {
+    print(func(1));
+}
